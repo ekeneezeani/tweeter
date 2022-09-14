b63af9fd4a537c5c34a8e28236b079834c456c27
ekeneezeani <108833282+ekeneezeani@users.noreply.github.com>
2022-09-14 17:46:39 -0500

    implementation of multiple tweets rendering

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,73 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function () {
 
+  const tweetData = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
+
+
+  const createTweetElement = function (obj) {
+    return `<article class ='tweet'>
+      <header> 
+        <div>  
+          <span class="pro-pic-name">
+            <span >
+              <img class="pro-pic" src= ${obj.user.avatars}>
+            </span>
+            <span class="pro-name">
+              ${obj.user.name}
+            </span>
+          </span>
+          <span >
+            <span class="tweeter-handle">${obj.user.handle}</span>
+          </span>
+        </div>   
+        <div>
+          <span class="tweet-content">${obj.content.text}</span>
+        </div>
+      </header>
+      <footer>
+        <span class="time-created">
+            ${obj.created_at}
+        </span>
+        <span>
+          <span class="flag"> <i class="fa-solid fa-flag"></i> </span>
+          <span class="retweet"> <i class="fa-solid fa-retweet"></i></span>
+          <span class="heart"> <i class="fa-solid fa-heart"></i></span>
+        </span> 
+      </footer>
+    </article>`;
+  };
+
+
+  const renderTweets = function (tweets) {
+    for (const tweet of tweets) {
+      const newTweetArticle = createTweetElement(tweet);
+      $("#tweet-container").append(newTweetArticle);
+      console.log(tweet)
+    }
+  };
+
+  renderTweets(tweetData);
+
+});//END OF DOCUMENT READY
